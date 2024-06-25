4d62976c389408020ade29d7f7428619a1473eaa
Dhanglerson Barral <dhanglerson.barral@crfsp.org.br>
2024-06-25 11:41:39 -0300

    Ambientes e Documentação

--- Data/BlogDataContext.cs
+++ Data/BlogDataContext.cs
@@ -5,12 +5,15 @@ using Microsoft.EntityFrameworkCore;
 namespace Blog.API.Data;
 public class BlogDataContext : DbContext
 {
+   public BlogDataContext(DbContextOptions<BlogDataContext> options)
+      : base(options)
+   {
+
+   }
+
    public DbSet<Category> Categories { get; set; }
    public DbSet<Post> Posts { get; set; }
    public DbSet<User> Users { get; set; }
-
-   protected override void OnConfiguring(DbContextOptionsBuilder options)
-      => options.UseSqlServer("Server=localhost,1433;Database=Blog;User ID=sa;Password=Barral#13;TrustServerCertificate=true");
 
    protected override void OnModelCreating(ModelBuilder modelBuilder)
    {
--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
 using System.IO.Compression;
 using System.Text;
 using System.Text.Json.Serialization;
-using Blog;
 using Blog.API;
 using Blog.API.Data;
 using Blog.API.Services;
@@ -14,14 +13,26 @@ ConfigureAuthentication(builder);
 ConfigureMvc(builder);
 ConfigureServices(builder);
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 LoadConfiguration(app);
 
+app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
-app.UseResponseCompression();
 app.MapControllers();
 app.UseStaticFiles();
+app.UseResponseCompression();
+
+if(app.Environment.IsDevelopment())
+{
+    Console.WriteLine("⌛Em ambiente de desenvolvimento!⏳");
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
 app.Run();
 
 void LoadConfiguration(WebApplication app)
@@ -81,7 +92,8 @@ void ConfigureMvc(WebApplicationBuilder builder)
 
 void ConfigureServices(WebApplicationBuilder builder)
 {
-    builder.Services.AddDbContext<BlogDataContext>();
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    builder.Services.AddDbContext<BlogDataContext>(options => options.UseSqlServer(connectionString));
     builder.Services.AddTransient<TokenService>();
     builder.Services.AddTransient<EmailService>();
 }
